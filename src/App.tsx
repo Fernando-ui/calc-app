4a2f1fede8a7072ad37748ff9638d3cafad9f203
Fernando <fst_96@hotmail.com>
2022-02-04 09:55:56 -0600

    Anadiendo codigo base con estilos y plantilla de calc

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "../src/styles/css/style.css";
+function App() {
+  return (
+    <>
+      <div className="calc">
+        <div className="calc__container">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione error odio dicta, amet voluptas, quo ullam nulla velit rerum omnis libero! Ab voluptas sequi esse et maxime culpa officia atque?
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
