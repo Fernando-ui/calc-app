a5c14decf5ca7b5d912add3d943ca2b61b5da524
Fernando <fst_96@hotmail.com>
2022-02-04 15:52:36 -0600

    Colocando span en el contenido de los span de los botones

--- src/components/Teclado.tsx
+++ src/components/Teclado.tsx
@@ -4,26 +4,66 @@ export const Teclado = () => {
   return (
     <>
       <div className="calc__container-button">
-        <div className="calc__button button-1">3</div>
-        <div className="calc__button button-2">3</div>
-        <div className="calc__button button-3">3</div>
-        <div className="calc__button button-4">3</div>
-        <div className="calc__button button-5">3</div>
-        <div className="calc__button button-6">3</div>
-        <div className="calc__button button-7">3</div>
-        <div className="calc__button button-8">3</div>
-        <div className="calc__button button-9">3</div>
-        <div className="calc__button button-10">3</div>
-        <div className="calc__button button-11">3</div>
-        <div className="calc__button button-12">3</div>
-        <div className="calc__button button-13">3</div>
-        <div className="calc__button button-14">3</div>
-        <div className="calc__button button-15">3</div>
-        <div className="calc__button button-16">3</div>
-        <div className="calc__button button-17">3</div>
-        <div className="calc__button button-18">3</div>
-        <div className="calc__button button-19">3</div>
-        <div className="calc__button button-20">3</div>
+        <div className="calc__button button-1">
+          <span className="calc__button--inf1">1</span>
+        </div>
+        <div className="calc__button button-2">
+          <span className="calc__button--inf2">2</span>
+        </div>
+        <div className="calc__button button-3">
+          <span className="calc__button--inf3">3</span>
+        </div>
+        <div className="calc__button button-4">
+          <span className="calc__button--inf4">4</span>
+        </div>
+        <div className="calc__button button-5">
+          <span className="calc__button--inf5">5</span>
+        </div>
+        <div className="calc__button button-6">
+          <span className="calc__button--inf6">6</span>
+        </div>
+        <div className="calc__button button-7">
+          <span className="calc__button--inf7">7</span>
+        </div>
+        <div className="calc__button button-8">
+          <span className="calc__button--inf8">8</span>
+        </div>
+        <div className="calc__button button-9">
+          <span className="calc__button--inf9">9</span>
+        </div>
+        <div className="calc__button button-10">
+          <span className="calc__button--info0">10</span>
+        </div>
+        <div className="calc__button button-11">
+          <span className="calc__button--info1">11</span>
+        </div>
+        <div className="calc__button button-12">
+          <span className="calc__button--info2">12</span>
+        </div>
+        <div className="calc__button button-13">
+          <span className="calc__button--info3">13</span>
+        </div>
+        <div className="calc__button button-14">
+          <span className="calc__button--info4">14</span>
+        </div>
+        <div className="calc__button button-15">
+          <span className="calc__button--info5">15</span>
+        </div>
+        <div className="calc__button button-16">
+          <span className="calc__button--info6">16</span>
+        </div>
+        <div className="calc__button button-17">
+          <span className="calc__button--info7">17</span>
+        </div>
+        <div className="calc__button button-18">
+          <span className="calc__button--info8">18</span>
+        </div>
+        <div className="calc__button button-19">
+          <span className="calc__button--info9">19</span>
+        </div>
+        <div className="calc__button button-20">
+          <span className="calc__button--info0">20</span>
+        </div>
       </div>
     </>
   );
