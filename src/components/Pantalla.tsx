bdc8ca49997df936a053424ea74ba3fc247725db
Fernando <fst_96@hotmail.com>
2022-02-08 11:30:07 -0600

    Anadiendo inputs modificados

--- src/components/Pantalla.tsx
+++ src/components/Pantalla.tsx
@@ -3,18 +3,28 @@ import React from "react";
 export const Pantalla = () => {
   return (
     <>
-      <div className="calc__screen">
-        <div className="calc__screen-header">
-          <span className="calc__screen-delete">Delete &lArr;</span>
+      <div className="calc__header">
+        <div className="calc__header-delete">
+          <span className="calc__header-button">Delete &lArr;</span>
         </div>
-        <div className="calc__screen-input">
-          <input type="number" className="calc__screen-input input-1" />
-          <input type="number" className="calc__screen-input input-2" />
-          <input type="number" className="calc__screen-input input-3" />
-          {/* // TODO Agregar al input 3 un before con un = para que no estorbe */}
+        <div className="calc__input">
+          <input
+            type="number"
+            disabled
+            className="calc__input-size calc__input-input-1"
+          />
+          <input
+            type="number"
+            disabled
+            className="calc__input-size calc__input-input-2"
+          />
+          <input
+            type="number"
+            disabled
+            className="calc__input-size calc__input-input-3"
+          />
         </div>
       </div>
     </>
   );
 };
-  
