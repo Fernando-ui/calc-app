0db565dc3b5cb1971befb8dfea66385b34b19b86
Fernando <fst_96@hotmail.com>
2022-02-08 10:23:31 -0600

    cambio de tamanio botones -Anadiendo boton borrar

--- src/components/Pantalla.tsx
+++ src/components/Pantalla.tsx
@@ -4,15 +4,17 @@ export const Pantalla = () => {
   return (
     <>
       <div className="calc__screen">
-        <div className="calc__screen-delete">
-          span.calc__screen-
+        <div className="calc__screen-header">
+          <span className="calc__screen-delete">Delete &lArr;</span>
         </div>
         <div className="calc__screen-input">
           <input type="number" className="calc__screen-input input-1" />
           <input type="number" className="calc__screen-input input-2" />
           <input type="number" className="calc__screen-input input-3" />
+          {/* // TODO Agregar al input 3 un before con un = para que no estorbe */}
         </div>
       </div>
     </>
   );
 };
+  
